e547f7f33500952b5be677d23636b2eaee24010a
Tássyo Santana <tassyo.santana@hotmail.com>
2015-10-08 19:19:39 -0300

    Persistência de restaurante(view edit;new;show)

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -7,4 +7,37 @@ class RestaurantesController < ApplicationController
 	def show
 		@restaurante = Restaurante.find(params[:id])
 	end
+
+	def destroy
+		@restaurante = Restaurante.find(params[:id])
+		@restaurante.destroy
+
+		redirect_to(action: "index")
+	end
+
+	def new
+		@restaurante = Restaurante.new
+	end
+
+	def restaurante_params
+		params.require(:restaurante).permit(:nome, :endereco, :especialidade)
+	end
+
+	def create
+		@restaurante = Restaurante.new(restaurante_params)
+		@restaurante.save
+
+		redirect_to(action: "show", id: @restaurante)
+	end
+
+	def edit
+		@restaurante = Restaurante.find(params[:id])
+	end
+
+	def update
+		@restaurante = Restaurante.find(params[:id])
+		@restaurante.update_attributes(restaurante_params)
+
+		redirect_to(action: "show", id: @restaurante)
+	end
 end
