6fc6fd8ee1c80d2f48b4f7845154837ccd226921
Tássyo Santana <tassyo.santana@hotmail.com>
2016-03-20 16:51:55 -0300

    Terminada a tela de edição de pratos e receitas

--- app/controllers/pratos_controller.rb
+++ app/controllers/pratos_controller.rb
@@ -17,6 +17,25 @@ class PratosController < ApplicationController
     end
 	end
 
+
+  def edit
+    @prato = Prato.find(params[:id])
+  end
+
+  def update
+    @prato = Prato.find(params[:id])
+
+    respond_to do |format|
+      if @prato.update(pratos_params)
+        flash[:notice] = 'ModelClassName was successfully updated.'
+        format.html { redirect_to(action:"index") }
+        format.xml  { head :ok }
+      else
+        format.html { render action: 'edit' }
+        format.xml  { render xml: @prato.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   private
 	def pratos_params
 		params.require(:prato).permit(:nome, :descricao)
--- app/controllers/receitas_controller.rb
+++ app/controllers/receitas_controller.rb
@@ -5,7 +5,7 @@ class ReceitasController < ApplicationController
 
 	def show
 	  @receita = Receita.find(params[:id])
-
+    @prato = Prato.find(@receita.prato_id)
 	  respond_to do |format|
 	    format.html # show.html.erb
 	    format.xml  { render xml: @receita }
@@ -27,7 +27,20 @@ class ReceitasController < ApplicationController
     @receita = Receita.find(params[:id])
   end
 
+def update
+  @receita = Receita.find(params[:id])
 
+  respond_to do |format|
+    if @receita.update(receitas_params)
+      flash[:notice] = 'ModelClassName was successfully updated.'
+      format.html { redirect_to(@receita) }
+      format.xml  { head :ok }
+    else
+      format.html { render action: 'edit' }
+      format.xml  { render xml: @receita.errors, status: :unprocessable_entity }
+    end
+  end
+end
   private
   def receitas_params
     params.require(:receita).permit(:modo_preparo,:ingredientes,:prato_id)
