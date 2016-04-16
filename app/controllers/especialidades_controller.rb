f015950ff64f848a72ff5505116d7b0fcbb760fc
Tássyo Santana <tassyo.santana@hotmail.com>
2016-04-16 00:46:09 -0300

    Controller Especialidade e views New e form

--- app/controllers/especialidades_controller.rb
+++ app/controllers/especialidades_controller.rb
@@ -17,4 +17,25 @@ class EspecialidadesController < ApplicationController
       format.xml  { render xml: @especialidade }
     end
   end
+
+  def create
+    @especialidade = Especialidade.new(especialidade_params)
+
+    respond_to do |format|
+      if @especialidade.save
+        flash[:notice] = 'ModelClassName was successfully created.'
+        format.html { redirect_to(@especialidade) }
+        format.xml  { render xml: @especialidade, status: :created, location: @especialidade }
+      else
+        format.html { render action: 'new' }
+        format.xml  { render xml: @especialidade.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def especialidade_params
+    params.require(:especialidade).permit()
+  end
+  
 end
