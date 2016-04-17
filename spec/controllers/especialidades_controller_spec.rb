51b50f6964fd0d46e476fdfe133039e30d1954a3
Tássyo Santana <tassyo.santana@hotmail.com>
2016-04-17 20:18:50 -0300

    Continuando o teste de cotroller especialidades

--- spec/controllers/especialidades_controller_spec.rb
+++ spec/controllers/especialidades_controller_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe EspecialidadesController, type: :controller do
-  let(:especialidade) { FactoryGirl.create(:especialidade)}
+  let(:esp) { FactoryGirl.create(:especialidade)}
 
   describe "GET #index" do
-    it 'Atribuir todas as especialidades para @especialidades' do
+    it 'atribuir todas as especialidades para @especialidades' do
       get :index
-      expect(assigns(:especialidades)).to eq([especialidade])
-     end
+      expect(assigns(:especialidades)).to eq([esp])
+    end
+    
+    it 'redirecionar para index.html.erb' do
+      get :index
+      expect(response).to render_template("index")
+    end
    end
+
+  describe "GET #new" do
+    it 'atribuir uma nova especialidade para @especialidade' do
+      get :new
+      expect(assigns(:especialidade)).to be_a_new(Especialidade) 
+    end 
+  end
+
+  describe "POST #create" do
+    it 'cria uma especialidade' do
+      
+  end
 end
